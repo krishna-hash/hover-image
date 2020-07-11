c0217effb468efa13fa3ddb2515ff706cebe12ea
krishnamoorthy.S <59560383+krishna-hash@users.noreply.github.com>
2020-07-11 04:01:03 -1200

    upload files

---
+++ hover image/my.js
@@ -0,0 +1,8 @@
+new hoverEffect({
+  parent: document.querySelector(".main-image"),
+  intensity: 0.3,
+  speedIn: 1.0,
+  image1: "images/one.jpg",
+  image2: "images/two.jpg",
+  displacementImage: "images/three.png",
+});
